70cdf9633a38c5ca929101c4d5d4badaea401563
EC2 Default User <ec2-user@ip-172-31-22-93.ap-northeast-1.compute.internal>
2023-06-04 13:13:45 +0000

    log-in後users/show.html.erbへ遷移

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,30 @@ class BooksController < ApplicationController
     @books = Books.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
   def index
-    @books = Book.page(params[:page])
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image, :opinion)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @books = Books.new
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
